9e863d75e539aee7fdbc55f05ce5e4e315600449
yogesh <yext@dotsquares.com>
2022-12-10 18:38:05 +0530

    upload files

--- src/templates/location.tsx
+++ src/templates/location.tsx
@@ -84,9 +84,9 @@ export const getPath: GetPath<TemplateProps> = ({ document }) => {
  * NOTE: This currently has no impact on the local dev path. Redirects will be setup on
  * a new deploy.
  */
-export const getRedirects: GetRedirects<TemplateProps> = ({ document }) => {
-  return [`index-old/${document.id.toString()}`];
-};
+// export const getRedirects: GetRedirects<TemplateProps> = ({ document }) => {
+//   return [`index-old/${document.id.toString()}`];
+// };
 
 /**
  * This allows the user to define a function which will take in their template
