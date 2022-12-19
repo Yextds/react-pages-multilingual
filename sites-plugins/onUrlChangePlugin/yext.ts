760a9573e2da5295f42f4088a573fd394ae7f626
Yogesh <yogesh.vishnoi@dotsquares.com>
2022-12-19 16:01:26 +0530

    Update On Url Change Plugin

--- sites-plugins/onUrlChangePlugin/yext.ts
+++ sites-plugins/onUrlChangePlugin/yext.ts
@@ -37,7 +37,7 @@ interface ApiResponse<T> {
 }
 
 const YEXT_API_KEY = "46061e82e51fa0626dbee0492d929fe1"; 
-const API_BASE = "https://api.yext.com/v2/accounts/me/";
+const API_BASE = "https://api-sandbox.yext.com/v2/accounts/me/";
 const VER = "20210714";
 
 function buildApiUrl(path: string, params?: Record<string, string>) {
