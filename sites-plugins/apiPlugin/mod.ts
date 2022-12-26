058e76903ab77a86f9418039b7e193c31fa67981
Yogesh <yogesh.vishnoi@dotsquares.com>
2022-12-26 12:06:04 +0530

    update api plugin

---
+++ sites-plugins/apiPlugin/mod.ts
@@ -0,0 +1,27 @@
+
+export interface apiPayload {
+  urlArgs: string;
+  userAgent: string;
+  requestUrl: string;
+  headers: any;
+}
+
+const YEXT_API_KEY = "46061e82e51fa0626dbee0492d929fe1"; 
+const API_BASE = "https://api-sandbox.yext.com/v2/accounts/me/";
+const VER = "20210714";
+
+function buildApiUrl(path: string, params?: Record<string, string>) {
+  const result = new URL(path, API_BASE);
+  result.searchParams.append("api_key", YEXT_API_KEY);
+  result.searchParams.append("v", VER);
+  for (const k in params) {
+    result.searchParams.append(k, params[k]);
+  }
+  return result.toString();
+}
+
+export function apiPlugin(data: apiPayload) {
+  
+  console.log('Entity data', data);
+  
+}
