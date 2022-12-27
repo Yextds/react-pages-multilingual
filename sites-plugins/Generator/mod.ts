96dd0bb26c3e82a685ade64b0b57bbda7405c0ad
Yogesh <yogesh.vishnoi@dotsquares.com>
2022-12-27 19:07:50 +0530

    added url

---
+++ sites-plugins/Generator/mod.ts
@@ -0,0 +1,14 @@
+import manifest from "../../dist/plugin/manifest.json" assert { type: "json" };
+// import { default as render } from "../../dist/assets/renderer/templateRenderer";
+import { default as render } from "./assets/renderer/templateRenderer.js";
+
+/**
+ * Generate simply executes the templateRenderer function bundled as part of yss build and returns
+ * the result.
+ */
+export const Generate = async (data): Promise<Record<any, any>> => {
+  return await render({
+    document: data.streamOutput,
+    __meta: { manifest },
+  });
+};
